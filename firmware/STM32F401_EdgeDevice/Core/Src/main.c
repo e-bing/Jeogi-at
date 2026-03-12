49b09606ac5993f21d4770d1d2c99cf1c03a0eb7
Jo-yechan <rhanfejddl00@gmail.com>
2026-03-12 10:51:20 +0900

    [firmware] refactor: 센서 init을 main에서 실행하게 수정

--- firmware/STM32F401_EdgeDevice/Core/Src/main.c
+++ firmware/STM32F401_EdgeDevice/Core/Src/main.c
@@ -125,8 +125,8 @@ int main(void)
   // test: sensor & motor
   // Start Timer with Interrupt
   //    HAL_TIM_Base_Start_IT(&htim3);
-  //    MQ135_Init();
-  //    MQ7_Init();
+  MQ135_Init();
+  MQ7_Init();
 
   // test: LED panel
   AppTask_Init();
